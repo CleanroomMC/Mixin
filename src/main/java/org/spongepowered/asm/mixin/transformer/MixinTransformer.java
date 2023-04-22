edc35a44dd9b19348bb0d87291c0b4ffa1b0c1c4
YZ Wu <gkappa@outlands.top>
2023-04-22 18:29:38 +0800

    public many methods

--- src/main/java/org/spongepowered/asm/mixin/transformer/MixinTransformer.java
+++ src/main/java/org/spongepowered/asm/mixin/transformer/MixinTransformer.java
@@ -42,7 +42,7 @@ import org.spongepowered.asm.util.asm.ASM;
 /**
  * Transformer which manages the mixin configuration and application process
  */
-final class MixinTransformer extends TreeTransformer implements IMixinTransformer {
+public final class MixinTransformer extends TreeTransformer implements IMixinTransformer {
     
     /**
      * Impl of mixin transformer factory
@@ -86,7 +86,7 @@ final class MixinTransformer extends TreeTransformer implements IMixinTransforme
     /**
      * Mixin processor which actually manages application of mixins
      */
-    private final MixinProcessor processor;
+    public final MixinProcessor processor;
     
     /**
      * Class generator 
